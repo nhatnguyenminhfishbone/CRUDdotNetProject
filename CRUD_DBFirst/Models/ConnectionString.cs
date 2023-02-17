747b173f57cd7c6ea732163132b7bddb4faac1e7
Nhat Nguyen Minh <nhat.nguyen.minh@preciofishbone.se>
2023-02-17 17:49:32 +0700

    First commit

---
+++ CRUD_DBFirst/Models/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_DBFirst.Models
+{
+    public class ConnectionString
+    {
+        private static string constr = "Data Source=VNMPCNHMI2; Initial Catalog=FileAndFolderDatabase;Trusted_Connection=True;MultipleActiveResultSets=true";
+        public static string Constr
+        {
+            get => constr;
+        }
+    }
+}
---
+++ CRUD_DBFirst/Models/tblEmployee .cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_DBFirst.Models
+{
+    [Table("tblEmployee")]
+    public class tblEmployee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
+        public string PhotoPath { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string Salary { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
